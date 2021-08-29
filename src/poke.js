6dc421ad5853cd462e3227805635eca53ea63c3e
Voltrex <mohammadkeyvanzade94@gmail.com>
2021-08-29 16:50:32 +0430

    src: refactor `poke.js`

--- src/poke.js
+++ src/poke.js
@@ -1,7 +1,6 @@
-const {Client} = require("discord.js"),
-      client = new Client();
- 
- 
-require("./utilities/loader.js")(client)
-require("./utilities/client.js")(client)
+const { Client } = require('discord.js');
 
+const client = new Client();
+
+require('./utilities/loader.js')(client);
+require('./utilities/client.js')(client);
